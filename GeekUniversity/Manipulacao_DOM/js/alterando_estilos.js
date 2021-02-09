e3cba3ced2fa29aadedfbb9842694981e86447d9
leonardobmessias <71788533+leonardobmessias@users.noreply.github.com>
2021-02-09 09:46:57 -0300

    minupação da dom

---
+++ GeekUniversity/Manipulacao_DOM/js/alterando_estilos.js
@@ -0,0 +1,19 @@
+let caixaElement = document.querySelector('.caixa')
+
+caixaElement.style.width = '200px'
+caixaElement.style.height ='200px'
+caixaElement.style.backgroundColor = 'green'
+
+/*
+As configuraçoes de estilos via JavaScript seguem
+os mesmo conceitos e atributos utilizados via css
+OBS: diferença
+Em CSS onde usa-se bacground-color 
+Em javaScript usa-se bacgroundColor
+
+Outro exemplo?
+Em CSS onde usa-se font-size
+Em JavaScript usa-se FontSize
+
+ou seja tira o ifem, juntas palavras e coloca as primeiras letras maiusculas
+*/
--- GeekUniversity/Manipulacao_DOM/working_with_of_dom.js
+++ GeekUniversity/Manipulacao_DOM/working_with_of_dom.js
@@ -1,33 +1,33 @@
 //let itexto = document.getElementById('produto') // pega o elemento pelo id
 //console.log(itexto)
 
-let spans = document.getElementsByTagName('span')// pega o elemento pela teg HTML obs: está no plural então pega todas as tags
+//let spans = document.getElementsByTagName('span')// pega o elemento pela teg HTML obs: está no plural então pega todas as tags
 //console.log(spans)
 
 //let eles = document.getElementsByClassName('texto')// pega todos os elementos que tem a clase declarada
 //console.log(eles)
 //console.log(spans[0].textContent)
 //console.log(spans[1].innerHTML)
-spans[0].textContent = 'Jesus'
+//spans[0].textContent = 'Jesus'
 //spans[1].innerHTML = 'Cristo'
 
-let span = document.getElementsByTagName('span')[0]
+//let span = document.getElementsByTagName('span')[0]
 //console.log(span.innerHTML)
 
-span.style.textTransform = 'uppercase'
+//span.style.textTransform = 'uppercase'
 
-let inp = document.querySelector('input') //Quere select é muito flexivel
+//let inp = document.querySelector('input') //Quere select é muito flexivel
 //console.log(inp)
-let eles = document.querySelector('.texto')//ele busca o primeiro elemento que ele encontrar
+//let eles = document.querySelector('.texto')//ele busca o primeiro elemento que ele encontrar
 //console.log(eles)
 
-let nos = document.querySelectorAll('.texto')// busca todos os elementos
+//let nos = document.querySelectorAll('.texto')// busca todos os elementos
 //console.log(nos)
 
-let div1 = document.querySelector('#div1') // busca o elemento pelo ide com seu filhos caso tenha
+//let div1 = document.querySelector('#div1') // busca o elemento pelo ide com seu filhos caso tenha
 //console.log(div1)
 
-let imp = document.querySelector('input[name=produto]')
+//let imp = document.querySelector('input[name=produto]')
 //console.log(imp)
 
 
@@ -39,7 +39,7 @@ let imp = document.querySelector('input[name=produto]')
 
 // Exemplo 2
 
-let btn = document.querySelector('buton.btn')
+let btn = document.querySelector('button.btn')
 let impu = document.querySelector('input[name=produto]')
 
 btn.onclick = function(){
